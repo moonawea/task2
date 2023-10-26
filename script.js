4448d9794f9556ec5a78501e0906f79c0bd942a3
diana <dianaoruzbaeva94@gmail.com>
2023-10-26 22:27:53 +0600

    task2 js

---
+++ script.js
@@ -0,0 +1,63 @@
+// вытащить Hello ivan
+// let a = 'hello'
+// let b = 'Ivan'
+// console.log ((a.replace('h', 'H') ), (b.replace('I','i')))
+
+// записать длину переменную в b
+// let a = 'lorem ipsum dolor'
+// let b = a.length;
+// console.log(b)
+
+//вернуть sit lorem ipsum dolor amet
+// let a = 'lorem ipsum dolor sit amet'
+// console.log(a.slice(17,21) + " " + a.slice(0,17));
+
+//вернуть CHILDREn
+// let a = "children"
+// let b = a.slice(0,7).toUpperCase() + "n"
+// console.log(b)
+
+// вернуть bsso
+// let a = "boss"
+// let b = a.replace('boss', 'bsso')
+// console.log(b)
+
+//вернуть index d
+// let a = "sunday"
+// let b = a.indexOf('d')
+// console.log(b)
+
+// вернуть hekso
+// let a = 'hello'
+// let b = a.replace('ll', 'ks')
+// console.log(b)
+
+// вернуть new us
+// let a = 'user'
+// let b = 'new'
+// let text = b + ' ' + a.slice(0,2)
+// console.log(text)
+
+// вернуть daydaymon
+// let a = 'monday'
+// let b = 'day'
+// let text = b + a.slice(3,6) + a.slice(0,3)
+// console.log(text)
+
+// вернуть 'hElLo'
+// let a = 'HELLO'
+// let text = a.replace('HELLO','hElLo')
+// console.log(text)
+
+// вернуть '450'
+// let a = 5
+// let b = '400'
+// let c = a * 10 + Number(b)
+// let d = c.toString()
+// console.log(d)
+
+// вернуть manak
+// let a = 'monkey'
+// let b = 'banana'
+// let c = b.slice(2, 6).replace('n', 'm') + 'k'
+// console.log(c)
